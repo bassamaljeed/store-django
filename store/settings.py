c614459c38c20d8caebf15fad9e054091c8e88ba
Bassam-home <aljeedjedbs@gmail.com>
2020-07-22 15:03:22 +0100

    add statecfiles

--- store/settings.py
+++ store/settings.py
@@ -133,8 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_URL = '/media/'  # يعني رابط الوسائط على الموقع
-# يعني مسار مجلد الوسائط على السيرفر
+MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
